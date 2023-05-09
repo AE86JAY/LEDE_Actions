830d69ac6c23eb9368db7e12a84606da64308ba9
AE86JAY <AE86JAY@users.noreply.github.com>
2023-05-09 18:30:06 +0800

    Update ap2.sh

--- ap2.sh
+++ ap2.sh
@@ -17,9 +17,9 @@ sed -i "s/hostname='OpenWrt'/hostname='LEDE'/g" ./package/base-files/files/bin/c
 sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 sed -i 's|<a href="https://github.com/openwrt/luci">Powered by <%|<a href="https://www.right.com.cn/forum/space-uid-671291.html">点我到恩山无线论坛<突破重韦> <%|g' ./feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
 # luci-app-easymesh
-rm -rf ./feeds/luci/applications/luci-app-easymesy
-git clone https://github.com/ntlf9t/luci-app-easymesh package/lean/luci-app-easymesh
-sed -i 's/wpad-openssl/wpad-mesh-openssl/g' package/lean/luci-app-easymesh/Makefile
+# rm -rf ./feeds/luci/applications/luci-app-easymesy
+# git clone https://github.com/ntlf9t/luci-app-easymesh package/lean/luci-app-easymesh
+# sed -i 's/wpad-openssl/wpad-mesh-openssl/g' package/lean/luci-app-easymesh/Makefile
 # git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall package/apps/passwall
 # git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall package/apps/luci-app-passwall
 # CONFIG_DEFAULT_luci-app-ssr-plus
