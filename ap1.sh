92b0581a3c0a2e13cd46e7de958c8698cb184c8a
AE86JAY <AE86JAY@users.noreply.github.com>
2023-05-10 00:23:30 +0800

    Update ap1.sh

--- ap1.sh
+++ ap1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-svn co ttps://github.com/vernesong/OpenClash.git pacakge/opecash
+svn co https://github.com/vernesong/OpenClash.git pacakge/opecash
