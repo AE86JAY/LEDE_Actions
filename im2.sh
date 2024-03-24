0874f6a0690386fb0348ada84d44e8e04c12bc0e
AE86JAY <AE86JAY@users.noreply.github.com>
2024-03-24 20:02:34 +0800

    添加important大佬的源代码

---
+++ im2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
+sed -i "s/set system.ntp.enable_server='1'/set system.ntp.enable_server='0'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='immortalwrt'/hostname='LEDE'/g" package/base-files/files/bin/config_generate
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+sed -i 's|<a class="luci-link" href="https://github.com/openwrt/luci"|<a class="luci-link" href="https://www.right.com.cn/forum/space-uid-671291.html"|g' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+sed -i 's/Powered by <%= ver.luciname %>/Powered by 恩山无线论坛_突破重韦_/g' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+rm -rf package/openwrt-packages/*argon*
