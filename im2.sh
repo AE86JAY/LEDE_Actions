642bfab60fa35eac73e09a605ac4f87b6caea243
AE86JAY <AE86JAY@users.noreply.github.com>
2024-03-31 21:34:10 +0800

    Update im2.sh

--- im2.sh
+++ im2.sh
@@ -13,5 +13,5 @@
 sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
 sed -i "s/set system.ntp.enable_server='1'/set system.ntp.enable_server='0'/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='Immortalwrt'/hostname='LEDE'/g" package/base-files/files/bin/config_generate
-sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
-sed -i 's|<a class="luci-link" href="https://github.com/openwrt/luci" target="_blank">Powered by <%= ver.luciname %> (<%= ver.luciversion %>)</a>|<a class="luci-link" href="https://v.douyin.com/iY1S8j1f/2@9.com:8pm" target="_blank">Powered by 抖音号：88169460799 突破重韦wei(<%= ver.luciversion %>)</a>|g' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+# sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+# sed -i 's|<a class="luci-link" href="https://github.com/openwrt/luci" target="_blank">Powered by <%= ver.luciname %> (<%= ver.luciversion %>)</a>|<a class="luci-link" href="https://v.douyin.com/iY1S8j1f/2@9.com:8pm" target="_blank">Powered by 抖音号：88169460799 突破重韦wei(<%= ver.luciversion %>)</a>|g' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
