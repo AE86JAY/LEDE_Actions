0747ebd0eb49c08c81016802f37537891ee95e74
AE86JAY <AE86JAY@users.noreply.github.com>
2024-07-27 12:33:12 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.65/g' package/base-files/files/bin/config_generate
 # sed -i "s/set system.ntp.enable_server='1'/set system.ntp.enable_server='0'/g" package/base-files/files/bin/config_generate
-sed -i "s/hostname='ImmortalWrt'/hostname='LEDE'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='openwrt'/hostname='LEDE'/g" package/base-files/files/bin/config_generate
 # sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 # sed -i 's|<a class="luci-link" href="https://github.com/openwrt/luci"|<a class="luci-link" href="https://www.right.com.cn/forum/space-uid-671291.html"|g' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
 # sed -i 's/Powered by <%= ver.luciname %>/Powered by 恩山无线论坛_突破重韦_/g' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
