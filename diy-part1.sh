921df2c3530973a7aafa983daeaf3e6affb7482e
AE86JAY <AE86JAY@users.noreply.github.com>
2024-07-28 16:41:53 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-git clone  https://github.com/AE86JAY/op-apps.git /package/opapps
+git clone https://github.com/AE86JAY/op-apps.git /package/opapps
 # git clone https://github.com/kiddin9/openwrt-packages.git package/diy1
 git clone # https://github.com/281677160/luci-app-autoupdate.git package/diy2
 # git clone https://github.com/haiibo/openwrt-packages package/openwrt-packages
