1c9eb665c527b5371eeba0f8fb8e655ade0a2bd9
AE86JAY <AE86JAY@users.noreply.github.com>
2024-07-29 20:36:34 +0800

    Add files via upload

--- im2.sh
+++ im2.sh
@@ -13,15 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.65/g' package/base-files/files/bin/config_generate   # 修改默认ip
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd    # 替换终端为bash
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile   # 选择argon为默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile   # 选择argon为默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile   # 选择argon为默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl/Makefile   # 选择argon为默认主题
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/99-default-settings    # 设置密码为空
-sed -i "s/hostname='Immortalwrt'/hostname='LEDE'/g" package/base-files/files/bin/config_generate
+sed -i 's/+uhttpd +uhttpd-mod-ubus //g' feeds/luci/collections/luci/Makefile    # 删除uhttpd
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings    # 设置密码为空
 # sed -i 's/PATCHVER:=5.10/PATCHVER:=5.15/g' target/linux/x86/Makefile   # x86机型,默认内核5.10，修改内核为5.15
 # rm -rf feeds/packages/utils/runc/Makefile   # 临时删除run1.0.3
 # svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile feeds/packages/utils/runc/Makefile   # 添加runc1.0.2
 # git clone --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
-rm -rf feeds/luci/themes/luci-theme-argon    # 删除自带argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon    # 替换新版argon
